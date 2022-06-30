626a771e623ae3421600f27f99317a2aa5ffe64c
Travis Gersch <t_gersch@hotmail.com>
2022-06-29 21:18:22 -0500

    first commit to git push

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"What is the name of the levitation spell?");
+
+        var spellName = Console.ReadLine();
+        
+        Console.WriteLine($"It's leviOsa, not {spellName}!");
+    }
+}
